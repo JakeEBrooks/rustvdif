bde3f181107f526e2e278ca69d36d65ff03dab20
Jacob Brooks <binarystar31297@googlemail.com>
2025-06-20 15:02:41 +0100

    add recv_vtp to receiver

--- src/recording/receiver.rs
+++ src/recording/receiver.rs
@@ -1,10 +1,12 @@
 use std::{mem, net::UdpSocket, os::fd::AsRawFd};
 
 use libc::{c_void, iovec, mmsghdr, recvmmsg, timespec};
-use tracing::{event, span, Level};
+use tracing::{event, Level};
 
 use crate::VDIFFrame;
 
+/// A high performance UDP packet receiver designed to handle large input data rates. Internally uses the [`recvmmsg`] system call
+/// to reduce the overhead of going through the OS
 pub struct VDIFReceiver {
     sock: UdpSocket,
     _frame_cap: usize,
@@ -13,7 +15,7 @@ pub struct VDIFReceiver {
 
     msgs: Box<[mmsghdr]>,
     _iovs: Box<[iovec]>,
-    bufs: Box<[Box<[u8]>]>,
+    pub(crate) bufs: Box<[Box<[u8]>]>,
     timeout: timespec
 }
 
@@ -35,28 +37,38 @@ impl VDIFReceiver {
         return Self { sock: socket, _frame_cap: vlen, frame_num: 0, frame_ind: 0, msgs: msgs, _iovs: iovs, bufs: bufs, timeout: timeout }
     }
 
-    pub fn recv_batch(&mut self) {
-        let span = span!(Level::DEBUG, "recvmmsg");
-        let _ = span.enter();
-
+    fn recv_batch(&mut self) {
         let res = unsafe { recvmmsg(self.sock.as_raw_fd(), self.msgs.as_mut_ptr(), self.msgs.len() as _, 0, &mut self.timeout) };
         if res < 0 {
             panic!("recvmmsg returned with error code: {}", res);
         }
-
+        event!(Level::DEBUG, "Received {res} frames from recvmmsg");
         self.frame_num = res.try_into().unwrap();
-        event!(Level::DEBUG, "Received {res} messages from recvmmsg");
     }
 
     pub fn recv_frame(&mut self) -> VDIFFrame {
         if self.frame_ind >= self.frame_num {
             self.recv_batch();
             self.frame_ind = 0;
-        }
+        };
 
         let outframe = VDIFFrame::from_byte_slice(&self.bufs[self.frame_ind]);
         self.frame_ind += 1;
 
         return outframe
+    }
+
+    pub fn recv_vtp_frame(&mut self) -> (u64, VDIFFrame) {
+        if self.frame_ind >= self.frame_num {
+            self.recv_batch();
+            self.frame_ind = 0;
+        };
+
+        let buf: &[u8] = &self.bufs[self.frame_ind];
+        let seq = u64::from_le_bytes(buf[0..8].try_into().unwrap());
+        let frame = VDIFFrame::from_byte_slice(&buf[8..]);
+        self.frame_ind += 1;
+
+        return (seq, frame)
     }
 }
